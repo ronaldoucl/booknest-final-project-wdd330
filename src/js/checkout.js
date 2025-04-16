f26e1e3b8b6c2412068f1e0e930ff92ac16f32a1
Ronaldo Campos <66402037+ronaldoucl@users.noreply.github.com>
2025-04-16 01:28:47 -0600

    checkout

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,10 +3,14 @@ import { getFromStorage, formatPrice, loadHeaderFooter } from './utils.js';
 loadHeaderFooter();
 
 const orderSummary = document.getElementById('orderSummary');
+const subtotalAmount = document.getElementById('subTotal');
+const taxAmount = document.getElementById('tax');
 const totalPaid = document.getElementById('totalPaid');
 
 function loadCheckout() {
   const cart = getFromStorage('cart');
+  let subtotal = 0;
+  let tax = 0;
   let total = 0;
 
   orderSummary.innerHTML = '';
@@ -18,13 +22,17 @@ function loadCheckout() {
 
   cart.forEach(item => {
     const price = (Math.random() * (30 - 9.99) + 9.99).toFixed(2);
-    total += parseFloat(price);
+    subtotal += parseFloat(price);
 
     const li = document.createElement('li');
     li.innerHTML = `<strong>${item.title}</strong> - ${formatPrice(price)}`;
     orderSummary.appendChild(li);
   });
 
+  tax = subtotal * 0.06;
+  total = subtotal + tax;
+  subtotalAmount.textContent = formatPrice(subtotal);
+  taxAmount.textContent = formatPrice(tax);
   totalPaid.textContent = formatPrice(total);
 
   localStorage.removeItem('cart');
